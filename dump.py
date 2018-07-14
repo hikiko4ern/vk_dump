57e622750d8d5840b25be3d3aafed6c211480588
Hikiko <bampir.rus@gmail.com>
2018-07-14 10:36:24 +0700

    v0.5.1 : messages txt support

--- dump.py
+++ dump.py
@@ -1,15 +1,17 @@
 ### Imports
 
-from time import sleep
-from os import makedirs, get_terminal_size
-from os.path import exists, join as pjoin
-from sys import stdout
-from urllib.request import urlretrieve, urlopen
 from math import ceil
+from os import get_terminal_size, makedirs
+from os.path import exists, join as pjoin
+from sys import stdout, stdin
+from time import sleep
+from urllib.request import urlopen, urlretrieve
 
 import vk_api
 
-VERSION = '0.4'
+from pprint import pprint
+
+VERSION = '0.5.1'
 API_VERSION = '5.80'
 
 REPLACE_SPACES = True # заменять пробелы на _ [в будущем будет возможность менять в настройках]
@@ -39,8 +41,8 @@ def log(*msg):
   global login, password, vk_session, vk
   cprint(msg[0] if msg else '[для продолжения необходимо войти]', color='red', mod='bold', offset=2, delay=1/50)
   try:
-    login = input('  login: \x1b[1;36m'); print('\x1b[0m',end='')
-    password = input('  password: \x1b[1;36m'); print('\x1b[0m',end='')
+    stdin.flush(); login = input('  login: \x1b[1;36m'); print('\x1b[0m',end='')
+    stdin.flush(); password = input('  password: \x1b[1;36m'); print('\x1b[0m',end='')
     vk_session = vk_api.VkApi(login, password, app_id=6631721, auth_handler=auth_handler, api_version=API_VERSION)
     vk_session.auth(token_only=True)
     vk = vk_session.get_api()
@@ -53,7 +55,7 @@ def log(*msg):
       raise e
 
 def auth_handler():
-  key = input('Введите код двухфакторой аутентификации: ')
+  stdin.flush(); key = input('Введите код двухфакторой аутентификации: ')
   remember_device = True
   return key, remember_device
 
@@ -87,10 +89,11 @@ def dump_photos():
     else:
       for r in range(ceil(count/1000)):
         for p in photos['items']:
-          print('    {}/{}'.format(i, count), end='\r')
+          print('\x1b[2K    {}/{}'.format(i, count), end='\r')
           download(p['sizes'][-1]['url'], folder)
           i += 1
         photos = vk.photos.get(album_id=al['id'], photo_sizes=1, count=1000, offset=(r+1)*1000)
+      print()
 
 
 
@@ -110,11 +113,12 @@ def dump_audio():
     print('  0/0')
   else:
     for a in audio:
-      print('  {}/{}'.format(i, count), end='\r')
+      print('\x1b[2K  {}/{}'.format(i, count), end='\r')
       download(a['url'], folder,
         name='{artist} - {title}_{id}'.format(artist=a['artist'], title=a['title'], id=a['id']),
         ext='mp3')
       i += 1
+    print()
 
 
 
@@ -130,7 +134,7 @@ def dump_video():
   albums = vk.video.getAlbums(count=100, need_system=1)
   albumsCount = albums['count']
 
-  for ar in range(ceil(albumsCount/100)):
+  for ar in range(ceil((albumsCount-100)/100)):
     for al in albums['items']:
       print('  Альбом "{}":'.format(al['title']))
       folder = pjoin('dump', 'video', '_'.join(al['title'].split(' '))); makedirs(folder, exist_ok=True)
@@ -139,15 +143,16 @@ def dump_video():
       if videoCount == 0:
         print('    0/0')
       else:
-        for vr in range(ceil(videoCount/200)):
+        for vr in range(ceil((videoCount-200)/200)):
           for v in video['items']:
-            print('    {}/{}'.format(i, videoCount), end='\r')
+            print('\x1b[2K    {}/{}'.format(i, videoCount), end='\r')
             download(
               research(b'https://cs.*vkuservideo.*'+str(v['height']).encode()+b'.mp4', urlopen(v['player']).read()).group(0).decode(),
               folder, name=v['title']+'_'+str(v['id']), ext='mp4')
               # во избежание конфликта имён к имени файла добавляется его ID
             i += 1
           video = vk.video.get(album_id=al['id'], count=200, offset=(vr+1)*200)
+        print()
     albums = vk.video.getAlbums(count=100, need_system=1, offset=(ar+1)*100)
 
 
@@ -159,10 +164,10 @@ def dump_docs():
   print('Сохраненние документов:')
   i, count = 1, docs['count']
   if count == 0:
-    print('\r  0/0')
+    print('\x1b[2K  0/0', end='\r')
   else:
     for d in docs['items']:
-      print('\r  {}/{}'.format(i, count), end='')
+      print('\x1b[2K  {}/{}'.format(i, count), end='\r')
       download(d['url'], folder, name=d['title']+'_'+str(d['id']), ext=d['ext'])
       # во избежание конфликта имён к имени файла добавляется его ID
       i += 1
@@ -170,7 +175,191 @@ def dump_docs():
 
 
 def dump_messages():
-  pass
+  def add_user(u):
+    if ('deactivated' in u) and (u['deactivated'] == 'deleted') and (u['first_name'] == 'DELETED'):
+      name = 'DELETED'
+      users[u['id']] = {'name': name, 'length': len(name)}
+    else:
+      name = u['first_name'] + ' ' + u['last_name']
+      users[u['id']] = {'name': name, 'length': len(name)}
+
+  def add_group(g):
+    name = g['name']
+    users[-g['id']] = {'name': name, 'length': len(name)}
+
+
+  def message_handler(msg):
+    """
+      Обработчик сообщений.
+      Возвращает массив строк.
+
+      [документация API]
+        [вложения]
+          [сообщениях]
+            - vk.com/dev/objects/attachments_m
+          [wall_reply]
+            - vk.com/dev/objects/attachments_w
+    """
+    r = []
+
+    if ('fwd_messages' in msg) and msg['fwd_messages']:
+      for fwd in msg['fwd_messages']:
+        res = message_handler(fwd)
+        if len(res) > 0:
+          if fwd['from_id'] not in users:
+            try:
+              add_user(vk.users.get(user_ids=fwd['from_id'])[0])
+            except Exception as e:
+              users[fwd['from_id']] = {'name': r'{unknown user}', 'length': 3}
+          
+          r.append('{name}> {}'.format(res[0], name=users.get(fwd['from_id'])['name']))
+          for m in res[1:]:
+            r.append('{name}> {}'.format(m, name=' '*len(users.get(fwd['from_id'])['name'])))
+
+    if len(msg['text']) > 0:
+      for line in msg['text'].split('\n'):
+        r.append(line)
+
+    if msg['attachments']:
+      for at in msg['attachments']:
+        tp = at['type']
+        if tp == 'photo':
+          r.append('[фото: {url}]'.format(url=at[tp]['sizes'][-1]['url']))
+        elif tp == 'video':
+          r.append('[видео: vk.com/video{owid}_{id}]'.format(owid=at[tp]['owner_id'], id=at[tp]['id']))
+        elif tp == 'audio':
+          r.append('[аудио: {artist} - {title}]'.format(artist=at[tp]['artist'], title=at[tp]['title']))
+        elif tp == 'doc':
+          r.append('[документ: vk.com/doc{owid}_{id}]'.format(owid=at[tp]['owner_id'], id=at[tp]['id']))
+        elif tp == 'link':
+          r.append('[ссылка: {title} ({url})]'.format(title=at[tp]['title'], url=at[tp]['url']))
+        elif tp == 'market':
+          r.append('[товар: {title} ({price}{cur}) [vk.com/market?w=product{owid}_{id}]]'.format(
+            title=at[tp]['title'],
+            owid=at[tp]['owner_id'],
+            id=at[tp]['id'],
+            price=at[tp]['price']['amount'],
+            cur=at[tp]['price']['currency']['name'].lower()))
+        # TODO: доделать market_album
+        elif tp == 'market_album':
+          r.append('[коллекция товаров: {title}]'.format(title=at[tp]['title']))
+        elif tp == 'wall':
+          r.append('[пост: vk.com/wall{owid}_{id}]'.format(owid=at[tp]['to_id'], id=at[tp]['id']))
+        # TODO: доделать wall_reply: добавить поддержку вложений (а надо ли?)
+        elif tp == 'wall_reply':
+          if at[tp]['from_id'] not in users:
+            add_user(vk.users.get(user_ids=at[tp]['from_id'])[0])
+          u = users.get(at[tp]['from_id'])
+          r.append('[комментарий к посту от {user}: {text} (vk.com/wall{owid}_{pid}?reply={id})]'.format(
+            user=u['name'],
+            text=at[tp]['text'],
+            owid=at[tp]['owner_id'],
+            pid=at[tp]['post_id'],
+            id=at[tp]['id']))
+        elif tp == 'sticker':
+          r.append('[стикер: {url}]'.format(url=at[tp]['images'][-1]['url']))
+        elif tp == 'gift':
+          r.append('[подарок: {id}]'.format(id=at[tp]['id']))
+        elif tp == 'graffiti':
+          r.append('[граффити: {url}]'.format(url=at[tp]['photo_604']))
+
+    return r
+
+
+  folder = pjoin('dump', 'dialogs')
+  makedirs(folder, exist_ok=True)
+
+  # get conversations
+  print('[получение диалогов...]')
+  print('\x1b[2K  0/???', end='\r')
+  conversations = vk.messages.getConversations(count=200, extended=1, fields='first_name, last_name, name')
+  if 'groups' not in conversations: conversations['groups'] = []
+  i, count = len(conversations['items']), conversations['count']
+  print('\x1b[2K  {}/{}'.format(i, count), end='\r')
+  for i in range(ceil((conversations['count']-200)/200)):
+    tmp = vk.messages.getConversations(count=200, offset=(i+1)*200, extended=1, fields='first_name, last_name, name')
+    conversations['items'] += tmp['items']
+    if 'profiles' in tmp:
+      for p in tmp['profiles']:
+        if p not in conversations['profiles']:
+          conversations['profiles'] += tmp['profiles']
+    if 'groups' in tmp:
+      for p in tmp['groups']:
+        if p not in conversations['groups']:
+          conversations['groups'] += tmp['groups']
+    i = len(conversations['items'])
+    print('\x1b[2K  {}/{}'.format(i, count), end='\r')
+  print('\n')
+
+
+  users = {}
+  for u in conversations['profiles']:
+    add_user(u)
+  for g in conversations['groups']:
+    add_group(g)
+
+  print('Сохранение диалогов:')
+  for con in conversations['items']:
+    did = con['conversation']['peer']['id']
+    
+    if con['conversation']['peer']['type'] == 'user':
+      if did not in users:
+        add_user(vk.users.get(user_ids=did)[0])
+      dialog_name = users.get(did)['name']
+    elif con['conversation']['peer']['type'] == 'group':
+      dialog_name = users.get(did)['name']
+    elif con['conversation']['peer']['type'] == 'chat':
+      dialog_name = con['conversation']['chat_settings']['title']
+    else:
+      dialog_name = r'{unknown}'
+    
+    # if exists(pjoin('dump', 'dialogs', '{}.txt'.format('_'.join(dialog_name.split(' '))))):
+    #   continue
+
+    print('  Диалог: {}'.format(dialog_name))
+    print('    [кэширование]')
+    print('\x1b[2K      0/???', end='\r')
+    history = vk.messages.getHistory(peer_id=con['conversation']['peer']['id'], count=200, rev=1, extended=1, fields='first_name,last_name')
+    i, count = len(history['items']), history['count']
+    print('\x1b[2K      {}/{}'.format(i, count), end='\r')
+    if 'profiles' not in history: history['profiles'] = []
+    for i in range(ceil((history['count']-200)/200)):
+      tmp = vk.messages.getHistory(peer_id=con['conversation']['peer']['id'], offset=(i+1)*200, count=200, rev=1, extended=1, fields='first_name,last_name')
+      history['items'] += tmp['items']
+      if 'profiles' in tmp:
+        for p in tmp['profiles']:
+          if p not in history['profiles']:
+            history['profiles'] += tmp['profiles']
+      i = len(history['items'])
+      print('\x1b[2K      {}/{}'.format(i, count), end='\r')
+    print()
+
+    # [..., {first_name, last_name, id} ...] => {%id%: {name: 'first_name + last_name', length: len(name) }
+    for u in history['profiles']:
+      if u['id'] not in users:
+        add_user(u)
+
+    # write history to .txt file
+    with open(pjoin('dump', 'dialogs', '{}_{id}.txt'.format('_'.join(dialog_name.split(' ')), id=did)), 'w+') as f:
+      count = len(history['items'])
+      print('    [сохранение]')
+      print('\x1b[2K      {}/{}'.format(0, count), end='\r')
+      prev = None
+      for i in range(count):
+        m = history['items'][i]
+        hold = ' '*(users.get(m['from_id'])['length']+2)
+        msg = hold if (prev and prev == m['from_id']) else users.get(m['from_id'])['name']+': '
+
+        res = message_handler(m)
+        if res:
+          msg += res[0] + '\n'
+          for r in res[1:]:
+            msg += hold + r + '\n'
+
+        f.write(msg)
+        prev = m['from_id']
+        print('\x1b[2K      {}/{}'.format(i+1, count), end='\r')
+    print(); print()
 
 
 
@@ -237,7 +426,8 @@ def menu():
     'Фото (по альбомам)', dump_photos,
     'Аудио', dump_audio,
     'Видео (по альбомам)', dump_video,
-    'Документы', dump_docs
+    'Документы', dump_docs,
+    'Сообщения', dump_messages
   ]
 
   print('Дамп данных:\n')
@@ -248,7 +438,7 @@ def menu():
 
   print()
   try:
-    choice = int(input('> ')); choice = exit if choice == 0 else actions[(choice-1)*2+1]
+    stdin.flush(); choice = int(input('> ')); choice = exit if choice == 0 else actions[(choice-1)*2+1]
     if choice is exit:
       goodbye()
     else:
